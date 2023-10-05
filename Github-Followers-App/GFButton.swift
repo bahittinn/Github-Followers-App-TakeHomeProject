82960d56e447e37a65d2a5bd32f57707e43a0ab2
bahittinn <bahittin84@gmail.com>
2023-10-05 15:53:47 +0300

    reuseable button and textfield

--- Github-Followers-App/GFButton.swift
+++ Github-Followers-App/GFButton.swift
@@ -11,16 +11,25 @@ class GFButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
     
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
     private func configure() {
-        layer.cornerRadius = 10
-        titleLabel?.textColor = .white
-        
+        layer.cornerRadius    = 10
+        titleLabel?.textColor  = .white
+        titleLabel?.font      = .preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
     }
     
 }
