0622d01e5a1baae9130e8225a02af442bde58279
bahittinn <bahittin84@gmail.com>
2023-10-08 22:32:00 +0300

    empty state view setup

---
+++ Github-Followers-App/CustomViews/Views/GFEmptyStateView.swift
@@ -0,0 +1,24 @@
+//
+//  GFEmptyStateView.swift
+//  Github-Followers-App
+//
+//  Created by Bahittin on 8.10.2023.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+    }
+    
+}
