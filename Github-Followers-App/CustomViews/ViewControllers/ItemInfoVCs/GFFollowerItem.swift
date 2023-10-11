e07ede02769c9463722c5a8f72c43cc7275ecd6b
bahittinn <bahittin84@gmail.com>
2023-10-11 13:49:06 +0300

    changes

---
+++ Github-Followers-App/CustomViews/ViewControllers/ItemInfoVCs/GFFollowerItem.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItem.swift
+//  Github-Followers-App
+//
+//  Created by Bahittin on 11.10.2023.
+//
+
+import UIKit
+
+class GFFollowerItem: GFItemInfoVC {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ Github-Followers-App/CustomViews/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  Github-Followers-App
+//
+//  Created by Bahittin on 11.10.2023.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
+    }
+}
